e47eec786f4c6c2f7709599e7d3384259d3a422c
kousu0530 <72140002+kousu0530@users.noreply.github.com>
2020-11-09 17:49:45 +0900

    メモ投稿機能のサーバーサイド実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,10 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("../checked")
 require("../memo")
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
